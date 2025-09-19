28672600c635ea5c58d6083ffa0d7405b104622a
duylt <lothanhduy2003@gmail.com>
2025-09-19 13:24:41 +0700

    WIP: Dashboard Screen UI -> Subject card section

---
+++ app/src/main/java/com/example/studysmart/domain/model/Subject.kt
@@ -0,0 +1,19 @@
+package com.example.studysmart.domain.model
+
+import androidx.compose.ui.graphics.Color
+import com.example.studysmart.presentation.theme.gradient1
+import com.example.studysmart.presentation.theme.gradient2
+import com.example.studysmart.presentation.theme.gradient3
+import com.example.studysmart.presentation.theme.gradient4
+import com.example.studysmart.presentation.theme.gradient5
+
+data class Subject(
+    var name: String,
+    var goalHours: Float,
+    var colors: List<Color>
+) {
+    companion object {
+        val subjectCardColors =
+            listOf(gradient1, gradient2, gradient3, gradient4, gradient5)
+    }
+}
--- app/src/main/java/com/example/studysmart/presentation/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/example/studysmart/presentation/dashboard/DashboardScreen.kt
@@ -1,26 +1,52 @@
 package com.example.studysmart.presentation.dashboard
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.studysmart.R
+import com.example.studysmart.domain.model.Subject
 import com.example.studysmart.presentation.components.CountCard
+import com.example.studysmart.presentation.components.SubjectCard
 
 @Composable
 fun DashboardScreen() {
+
+    val dummySubjectData = listOf(
+        Subject("English", 10f, Subject.subjectCardColors[0]),
+        Subject("Physic", 10f, Subject.subjectCardColors[1]),
+        Subject("Math", 10f, Subject.subjectCardColors[2]),
+        Subject("Fine Arts", 10f, Subject.subjectCardColors[3]),
+        Subject("Music", 10f, Subject.subjectCardColors[4]),
+    )
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -40,6 +66,13 @@ fun DashboardScreen() {
                     subjectCount = 10,
                     studiedHours = 10,
                     goalStudyHours = 10
+                )
+            }
+
+            item {
+                SubjectCardSection(
+                    modifier = Modifier.fillMaxWidth(),
+                    subjectsList = dummySubjectData
                 )
             }
         }
@@ -86,5 +119,73 @@ private fun CountCardsSection(
             headingText = stringResource(R.string.goal_study_hours),
             count = goalStudyHours
         )
+    }
+}
+
+@Composable
+private fun SubjectCardSection(
+    modifier: Modifier = Modifier,
+    subjectsList: List<Subject>,
+    emptyListTextRes: Int = R.string.msg_empty_subjects
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                modifier = Modifier.padding(
+                    start = 12.dp
+                ),
+                text = stringResource(R.string.subjects).uppercase(),
+                style = MaterialTheme.typography.bodySmall
+            )
+
+            IconButton(
+                onClick = {
+
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.add_subject)
+                )
+            }
+        }
+
+        if (subjectsList.isEmpty()) {
+            Image(
+                modifier = Modifier.size(120.dp)
+                    .align(Alignment.CenterHorizontally),
+                painter = painterResource(R.drawable.img_books),
+                contentDescription = stringResource(emptyListTextRes)
+            )
+
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(emptyListTextRes),
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Gray,
+                textAlign = TextAlign.Center
+            )
+        }
+
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(horizontal = 12.dp)
+        ) {
+            items(subjectsList) { subject ->
+                SubjectCard(
+                    subjectName = subject.name,
+                    gradientColors = subject.colors,
+                    onClick = {
+
+                    }
+                )
+            }
+        }
     }
 }
