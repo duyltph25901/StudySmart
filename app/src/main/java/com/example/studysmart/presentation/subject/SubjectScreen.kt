b58765686e859bbe60ddf586b3e3db349caa2e95
duylt <lothanhduy2003@gmail.com>
2025-09-23 16:47:39 +0700

    WIP: task UI subject screen - subject overt view section

--- app/src/main/java/com/example/studysmart/presentation/subject/SubjectScreen.kt
+++ app/src/main/java/com/example/studysmart/presentation/subject/SubjectScreen.kt
@@ -1,12 +1,20 @@
 package com.example.studysmart.presentation.subject
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,10 +23,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.example.studysmart.R
+import com.example.studysmart.presentation.components.CountCard
 
 @Composable
 fun SubjectScreen() {
@@ -37,10 +50,18 @@ fun SubjectScreen() {
         }
     ) { innerPadding ->
         LazyColumn(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxSize()
                 .padding(innerPadding)
         ){
-
+            item {
+                SubjectOverViewSection(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(12.dp ),
+                    studiedHours = 10,
+                    goalHours = 10,
+                    progress = 0.75f
+                )
+            }
         }
     }
 }
@@ -96,4 +117,62 @@ fun SubjectScreenTopBar(
             }
         }
     )
+}
+
+@Composable
+fun SubjectOverViewSection(
+    modifier: Modifier = Modifier,
+    studiedHours: Int ,
+    goalHours: Int,
+    progress: Float,
+) {
+    val percentageProgress = remember(progress) {
+        (progress * 100).toInt().coerceIn(0, 100)
+    }
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = stringResource(R.string.goal_study_hours),
+            count = goalHours
+        )
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = stringResource(R.string.study_hours),
+            count = studiedHours
+        )
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Box(
+            modifier = Modifier.size(75.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.fillMaxSize(),
+                progress = 1f,
+                strokeWidth = 4.dp,
+                strokeCap = StrokeCap.Round,
+                color = MaterialTheme.colorScheme.surfaceVariant
+            )
+
+            CircularProgressIndicator(
+                modifier = Modifier.fillMaxSize(),
+                progress = progress,
+                strokeWidth = 4.dp,
+                strokeCap = StrokeCap.Round,
+            )
+
+            Text(
+                text = "$percentageProgress%"
+            )
+        }
+    }
 }
